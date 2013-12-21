465ef9852c2ce4efc62dc97303cd236b1659904b
JackonYang <jiekunyang@gmail.com>
2013-12-21 11:00:30 +0800

    mFileMeta info

--- model.py
+++ model.py
@@ -1,10 +1,10 @@
 import json
 
 
-class FlatFile:
-    """Flat File meta
+class FileMeta:
+    """meta info of a flat file
     
-    @attr file_id: file id. md5.ext. immuttable;
+    @attr file_id: file id, immuttable. for example: md5.ext.
     @attr rawnames: set of raw filenames. support add only;
     @attr dispname: str of display name. only 1 name is supported. support update only;
     @attr status: int flat of status. support update only.
@@ -38,14 +38,14 @@ class FlatFile:
         return json.dumps([self.file_id, list(self.rawnames), self.dispname, self.status])
 
 if __name__ == '__main__':
-    obj_a = FlatFile('aas2342.pdf', 'hello.pdf')
+    obj_a = FileMeta('aas2342.pdf', 'hello.pdf')
     print obj_a
     obj_a.update_status(2)
     obj_a.update_dispname('hello world.pdf')
     obj_a.add_rawname('world.pdf')
 
-    new_obj_a = str(FlatFile(*json.loads(str(obj_a))))
+    new_obj_a = str(FileMeta(*json.loads(str(obj_a))))
 
-    obj_b = FlatFile('aas2342.pdf', ['hello.pdf', 'world.pdf', 'hello.pdf'], 'hello world.pdf', 2)
+    obj_b = FileMeta('aas2342.pdf', ['hello.pdf', 'world.pdf', 'hello.pdf'], 'hello world.pdf', 2)
     print new_obj_a
     print new_obj_a == str(obj_b)
