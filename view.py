980d1dcac219a2034ac63d38e361bb7ef5ca5ad0
JackonYang <jiekunyang@gmail.com>
2013-12-22 12:10:19 +0800

    support search

--- view.py
+++ view.py
@@ -4,10 +4,11 @@
 import wx
 
 from ObjectListView import ObjectListView, ColumnDefn
+from ObjectListView import Filter
 
 from controls import FlatFile
 
-class MyFrame(wx.Frame):
+class FlatFileFrame(wx.Frame):
     def __init__(self, file_repo, *args, **kwds):
         wx.Frame.__init__(self, *args, **kwds)
         self.file_repo = file_repo
@@ -17,6 +18,7 @@ class MyFrame(wx.Frame):
         self.InitModel()
         self.InitWidgets()
         self.InitObjectListView()
+        self.InitSearchCtrls()
 
     def InitModel(self):
         self.files = self.file_repo.get_filemeta()
@@ -31,6 +33,8 @@ class MyFrame(wx.Frame):
         sizer_2 = wx.BoxSizer(wx.VERTICAL)
         sizer_2.Add(self.myOlv, 1, wx.ALL|wx.EXPAND, 4)
         panel.SetSizer(sizer_2)
+
+        self.SearchFile = wx.SearchCtrl(panel)
 
         self.Layout()
 
@@ -47,6 +51,28 @@ class MyFrame(wx.Frame):
     def OnOpenFile(self, event):
         obj = self.myOlv.GetSelectedObject()
         self.file_repo.open_file(obj.file_id)
+
+    def OnTextSearchCtrl(self, event, searchCtrl, olv):
+        searchCtrl.ShowCancelButton(len(searchCtrl.GetValue()))
+        olv.GetFilter().SetText(searchCtrl.GetValue())
+        olv.RepopulateList()
+
+    def OnCancelSearchCtrl(self, event, searchCtrl, olv):
+        searchCtrl.SetValue("")
+        self.OnTextSearchCtrl(event, searchCtrl, olv)
+
+    def InitSearchCtrls(self):
+        """Initialize the search controls"""
+        for (searchCtrl, olv) in [(self.SearchFile, self.myOlv)]:
+            # Use default parameters to pass extra information to the event handler
+            def _handleText(evt, searchCtrl=searchCtrl, olv=olv):
+                self.OnTextSearchCtrl(evt, searchCtrl, olv)
+            def _handleCancel(evt, searchCtrl=searchCtrl, olv=olv):
+                self.OnCancelSearchCtrl(evt, searchCtrl, olv)
+            searchCtrl.Bind(wx.EVT_TEXT, _handleText)
+            searchCtrl.Bind(wx.EVT_SEARCHCTRL_CANCEL_BTN, _handleCancel)
+            olv.SetFilter(Filter.TextSearch(olv, olv.columns[0:4]))
+
         
         #open_file(os.path.join(repo_path, self.idx2name[event.GetIndex()]))
 
@@ -61,7 +87,7 @@ if __name__ == '__main__':
     app = wx.PySimpleApp(1)
     wx.InitAllImageHandlers()
 
-    frame_1 = MyFrame(repo, None, -1, "Flat File Explorer")
+    frame_1 = FlatFileFrame(repo, None, -1, "Flat File Explorer")
     app.SetTopWindow(frame_1)
     frame_1.Show()
     app.MainLoop()
