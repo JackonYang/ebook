df71291568547eaca2fe4b81c8a62fec9447a15d
JackonYang <jiekunyang@gmail.com>
2014-01-07 23:48:51 +0800

    BookMeta, 1 meta file for 1 book

---
+++ bookmeta.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8-*-
+"""book meta info
+
+1 json file for 1 book to store meta info
+@necessary attr: file_id, file_ext, rawname, dispname
+
+abspath is used,
+use get_metafile to get abspath of a file_id or json file
+
+install_repo before use BookMeta
+
+rawname is save in set and show in str.
+if dispname is None, display rawname without any influence on storage.
+
+"""
+import os
+import sys
+import json
+import codecs
+import time
+
+_root_path = None
+_auto_save = True
+_time_fmt = '%Y-%m-%d %H:%M:%S'
+_json_kwargs = {'indent': 4,
+                'separators': (',', ': '),
+                'encoding': 'utf8',
+                'sort_keys': True,
+                }
+
+
+def install_repo(root_path, auto_save=True):
+    """install root_path to save meta info
+
+    """
+    global _root_path
+    global _auto_save
+    _root_path = os.path.abspath(root_path)
+    if not os.path.exists(_root_path):
+        os.makedirs(_root_path)
+    _auto_save = auto_save
+
+
+def get_metafile(file_id):
+    if file_id.startswith(_root_path):
+        return file_id
+    if file_id.endswith('.json'):
+        fname = file_id
+    else:
+        fname = '%s.json' % file_id
+    return os.path.join(_root_path, fname)
+
+
+def _save_meta(meta_obj):
+    """save meta info with name file_id.json
+
+    """
+    if _root_path is None:
+        raise NameError('root path for BookMeta is not defined')
+
+    with codecs.open(get_metafile(meta_obj.file_id), 'w', 'utf8') as f:
+        f.write(unicode(meta_obj))
+
+
+def load(metafile):
+    with codecs.open(get_metafile(metafile), 'r', 'utf8') as f:
+        content = f.read()
+    return BookMeta(**json.loads(content, encoding='utf8'))
+
+
+def _clean_str(string):
+    """convert utf8 unicode if string is str
+
+    filesystemencoding is used to decode
+
+    """
+    if isinstance(string, str):
+        string = string.decode(sys.getfilesystemencoding())
+    return string
+
+
+class BookMeta:
+    def __init__(self, file_id, file_ext, rawname, dispname, **additional):
+        # make sure that all inputs are utf-8 encoding
+        self.__dict__['file_id'] = _clean_str(file_id)
+        self.__dict__['file_ext'] = _clean_str(file_ext)
+        self.__dict__['dispname'] = _clean_str(dispname)
+
+        rawname = _clean_str(rawname)
+        if isinstance(rawname, unicode):
+            self.__dict__['rawname'] = {rawname}
+        else:
+            self.__dict__['rawname'] = set(rawname)
+
+        self.__dict__['additional'] = {
+            _clean_str(key): _clean_str(value)
+            for key, value in additional.items()
+        }
+        if _auto_save:
+            _save_meta(self)
+
+    def __unicode__(self):
+        obj = {'file_id': self.file_id,
+               'rawname': list(self.rawname),
+               'dispname': self.dispname,
+               'file_ext': self.file_ext,
+               }
+        obj.update(self.additional)
+        return json.dumps(obj, **_json_kwargs)
+
+    def __setattr__(self, name, value, save=None):
+        self.__dict__[name] = value
+        if _auto_save:
+            _save_meta(self)
+
+    def get_rawname(self):
+        return ','.join(self.rawname)
+
+    def add_rawname(self, rawname):
+        rawname = _clean_str(rawname)
+        if isinstance(rawname, unicode):
+            rawname = {rawname}
+        self.rawname.update(rawname)
+
+    def get_dispname(self):
+        if not self.dispname:
+            return self.get_rawname()
+        return self.dispname
+
+    def get_create_time(self):
+        return time.strftime(
+            _time_fmt,
+            time.localtime(self._get_additional_attr('create_time', 0))
+        )
+
+    def get_sizeInMb(self):
+        return self._get_additional_attr('sizeInBytes', 0) / (1024.0*1024.0)
+
+    def _get_additional_attr(self, attr, default):
+        return self.additional.get(attr, default)
+
+
+if __name__ == '__main__':
+    install_repo('test_repo')
+    a = BookMeta(
+        'hello.sfd', 'pdf', 'rawname_a', None,  # required attr
+        a=4, create_time=time.time()  # additional attr
+    )
+    b = BookMeta(
+        'worldccc', 'pdf', ['rawname2', 'rawname3'], 'hello world',
+        sizeInBytes=1024000
+    )
+    c = load(os.path.join(_root_path, 'worldccc.json'))
+    d = load('worldccc')
+    print unicode(c)
+    print unicode(c) == unicode(d)
+    print a.get_dispname()
+    print a.dispname
+    print a.get_create_time()
+    print b.get_create_time()
+    print '%.2f(Mb)' % b.get_sizeInMb()
+    print '%.2f(Mb)' % a.get_sizeInMb()
