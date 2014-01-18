0534adcb2458405242fb8844bd20674bdcdb615d
JackonYang <jiekunyang@gmail.com>
2014-01-18 21:29:42 +0800

    model adopt to storage.py

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8-*-
+
 """model for book meta info
 
 book meta info saved in json file, 1 json file for 1 book.
@@ -13,34 +14,18 @@ rawname is save in set and show in str.
 if dispname is None, show rawname without any influence on storage.
 
 """
-import os
 import sys
 import json
-import codecs
 import time
 
 
 _auto_save = True  # when setattr
-_use_abs_path = True
 _time_fmt = '%Y-%m-%d %H:%M:%S'
 _json_kwargs = {'indent': 4,
                 'separators': (',', ': '),
                 'encoding': 'utf8',
                 'sort_keys': True,
                 }
-
-
-def init_mng(root_path):
-    """install root_path to save meta info
-
-    """
-    if _use_abs_path:
-        root_path = os.path.abspath(root_path)
-    if not os.path.exists(root_path):
-        os.makedirs(root_path)
-    mng = MetaMng(root_path)
-    BookMeta.mng = mng
-    return mng
 
 
 def _clean_str(string):
@@ -54,56 +39,14 @@ def _clean_str(string):
     return string
 
 
-class MetaMng:
-
-    def __init__(self, meta_root):
-        self.meta_root = meta_root
-
-    def filepath(self, metafile):
-        """get abspath of metafile
-
-        @para metafile: string is required. abspath/json file/file_id is OK.
-
-        """
-        if metafile.startswith(self.meta_root):
-            return metafile
-        if not metafile.endswith('.json'):  # file_id
-            metafile = '%s.json' % metafile
-        return os.path.join(self.meta_root, metafile)
-
-    def load(self, filename):
-        with codecs.open(filename, 'r', 'utf8') as f:
-            content = f.read()
-        return BookMeta.parse(content)
-
-    def save(self, meta_obj):
-        with codecs.open(self.filepath(meta_obj.file_id), 'w', 'utf8') as f:
-            f.write(unicode(meta_obj))
-    
-    def delete(self, meta_obj):
-        # delete meta file
-        try:
-            os.remove(self.filepath(meta_obj.file_id))
-        except IOError:
-            return 1
-        return None
-
-    def meta_exists(self, file_id):
-        return os.path.isfile(self.filepath(file_id))
-
-    def get_all(self):
-        metafiles = os.listdir(self.meta_root)
-        return [self.load(self.filepath(fname)) for fname in metafiles]
-
-
 class BookMeta:
     """Meta info of a single book
 
-    init a manager, and it saves data once attribute changed.
+    init a storage, and it saves data on attribute changed.
 
     """
 
-    mng = None
+    repo = None
 
     def __init__(self, file_id, file_ext, rawname, dispname, **additional):
         # make sure that all inputs are utf-8 encoding
@@ -123,7 +66,7 @@ class BookMeta:
         }
 
     @classmethod
-    def parse(cls, content):
+    def feed(cls, content):
         """unicode str to obj"""
         return BookMeta(**json.loads(content, encoding='utf8'))
 
@@ -139,8 +82,8 @@ class BookMeta:
 
     def __setattr__(self, name, value):
         self.__dict__[name] = value
-        if _auto_save and self.mng and hasattr(self.mng, 'save'):
-            self.mng.save(self)
+        if _auto_save and self.repo and hasattr(self.repo, 'save'):
+            self.repo.save(self.file_id, unicode(self))
 
     def get_rawname(self):
         return ','.join(self.rawname)
@@ -156,6 +99,9 @@ class BookMeta:
             return self.get_rawname()
         return self.dispname
 
+    def set_dispname(self, dispname):
+        self.dispname = dispname
+
     def get_create_time(self):
         return time.strftime(
             _time_fmt,
@@ -170,26 +116,18 @@ class BookMeta:
 
 
 if __name__ == '__main__':
-    mng = init_mng('test_repo')
     a = BookMeta(
         'hello.sfd', 'pdf', 'rawname_a', None,  # required attr
         a=4, create_time=time.time()  # additional attr
     )
-    a.mng.save(a)
     b = BookMeta(
         'worldccc', 'pdf', ['rawname2', 'rawname3'], 'hello world',
         sizeInBytes=1024000
     )
-    mng.save(b)
-    print mng.get_all()
-    c = mng.load(mng.filepath('worldccc'))
-    print unicode(c)
-    print unicode(c) == unicode(b)
+    print unicode(a)
     print a.get_dispname()
     print a.dispname
     print a.get_create_time()
     print b.get_create_time()
     print '%.2f(Mb)' % b.get_sizeInMb()
     print '%.2f(Mb)' % a.get_sizeInMb()
-    print mng.meta_exists('worldccc')
-    print mng.meta_exists('aaaaa')
