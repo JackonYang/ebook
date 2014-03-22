b0fd347f1ecb18aba9eb01c7161faeeb1f8f1f6b
JackonYang <jiekunyang@gmail.com>
2014-03-22 13:37:46 +0800

    mongo 存储图书元信息

--- settings.py
+++ settings.py
@@ -1,10 +1,8 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-repo_path = os.path.join(BASE_DIR, 'booklist')
-metafile_path = os.path.join(repo_path, 'metainfo')
-bookfile_path = os.path.join(repo_path, 'bookfile')
-cache_path = os.path.join(repo_path, 'cache')
+db_ip = "localhost"
+db_port = 27017
+db_name = 'data_bang'
+ignore_seq = {'.git', 'log', 'logs'}
+media_path = "/media/document/books"
 
 try:
     from local_settings import *
